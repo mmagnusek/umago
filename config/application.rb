c26b34592270b2ad1e9a657482d3767ece4c9e5e
Martin Magnusek <magnusekm@gmail.com>
2026-01-04 20:15:12 +0100

    Disable active storage

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Umago
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.active_storage.variant_processor = :disabled
+
     config.i18n.load_path += Dir[Rails.root.join 'config/locales/**/*.{rb,yml}']
     config.i18n.available_locales = [:cs]
     config.i18n.default_locale = :cs
